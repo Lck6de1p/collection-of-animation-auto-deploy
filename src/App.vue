29d66acf663ad0e6501005170c0b9991c5e31c0f
Lck6de1p <310504719@qq.com>
2021-02-04 16:39:44 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component1 />
+    <component2 />
+    <component3 />
+    <component4 />
+    <component5 />
+    <component6 />
+    <component7 />
   </div>
 </template>
 
-<style>
+<script>
+import component1 from '@/components/朋克风按钮.vue'
+import component2 from '@/components/打字机效果.vue'
+import component3 from '@/components/水球动画.vue'
+import component4 from '@/components/橡皮擦效果.vue'
+import component5 from '@/components/卡片集.vue'
+import component6 from '@/components/手风琴效果.vue'
+import component7 from '@/components/文字渐入效果.vue'
+
+export default {
+  components: {
+    component1,
+    component2,
+    component3,
+    component4,
+    component5,
+    component6,
+    component7
+  }
+}
+</script>
+<style lang='scss'>
+body, html, #app {
+  width: 100%;
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  overflow-y: auto;
 }
-
-#nav {
-  padding: 30px;
+::-webkit-scrollbar{
+  width: 5px;
+  height: 5px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+::-webkit-scrollbar-thumb{
+  border-radius: 1em;
+  background-color: rgba(50,50,50,.3);
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+::-webkit-scrollbar-track{
+  border-radius: 1em;
+  background-color: rgba(50,50,50,.1);
+}
+.css-demo {
+  position: relative;
+  border-top: 1px solid #ccc;
+  &::after {
+    content: attr(data-title);
+    position: absolute;
+    left: 50%;
+    top: 0;
+    padding: 0 12px;
+    color: #fff;
+    line-height: 30px;
+    background-color: #1e9fff;
+    border-radius: 0 0 5px 5px;
+    transform: translateX(-50%);
+  }
 }
 </style>
+
